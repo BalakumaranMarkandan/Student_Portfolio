1063e556dd4fbd522d41eb2a2191fc696f02743a
BalakumaranMarkandan <balakumaran.markandan@sorimtechnologies.com>
2026-05-19 23:28:07 +0530

    fixing app startup issue

--- src/main/java/com/klef/jfsd/project/StudentProjectAndPortfolioManagementApplication.java
+++ src/main/java/com/klef/jfsd/project/StudentProjectAndPortfolioManagementApplication.java
@@ -3,7 +3,8 @@ package com.klef.jfsd.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "com.klef.jfsd.project")
 public class StudentProjectAndPortfolioManagementApplication {
 
 	public static void main(String[] args) {
